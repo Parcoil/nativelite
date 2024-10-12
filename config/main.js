068e2548844f81c0d7748d25ee635c310bde5045
GZZGSOLAE <ttricky.vr@gmail.com>
2024-10-12 17:37:11 -0400

    main.js

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
+var sitename = "GZZ6."; // Change this to change the name of your website.
 var subtext = "v1.2"; // set the subtext
 
 // more settings in main.css
