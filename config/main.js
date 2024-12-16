06759c1b6eb3abcbd62452e42fe37714ca6b1371
joeskib <146056538+sdfsdfasdfsdfasdfsdfasdf@users.noreply.github.com>
2024-12-15 18:10:31 -0600

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "SuperNova."; // Change this to change the name of your website.
+var subtext = "Made by a 7th grader in AMS"; // set the subtext
 
 // more settings in main.css
 
